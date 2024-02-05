7c9050ca16acbe9a4ad1a6ec5fc5c1664e4c9fa9
frpoteau <152607603+frpoteau@users.noreply.github.com>
2024-02-05 16:31:28 +0100

    Liste des tables à réaliser

---
+++ public/Support/liste_des_tables.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *  TABLES A FAIRE :
+ *      - user
+ *      - participant
+ *      - campus
+ *      - sortie
+ *      - état (créer ouvert annuler cloturer en-cours)
+ *      - lieux
+ *      - ville (de la sortie)
+ *
+ *
+ */
