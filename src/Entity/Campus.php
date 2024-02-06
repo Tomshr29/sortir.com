57d5aaa0dabde75215c57ee7fbb3cf6c1a13fe33
Tom Scherer <106777338+Tomshr29@users.noreply.github.com>
2024-02-06 16:43:41 +0100

    Campus & état

---
+++ src/Entity/Campus.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CampusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CampusRepository::class)]
+class Campus
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idCampus = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdCampus(): ?int
+    {
+        return $this->idCampus;
+    }
+
+    public function setIdCampus(int $idCampus): static
+    {
+        $this->idCampus = $idCampus;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Shape.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShapeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShapeRepository::class)]
+class Shape
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idEtat = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $wording = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdEtat(): ?int
+    {
+        return $this->idEtat;
+    }
+
+    public function setIdEtat(int $idEtat): static
+    {
+        $this->idEtat = $idEtat;
+
+        return $this;
+    }
+
+    public function getWording(): ?string
+    {
+        return $this->wording;
+    }
+
+    public function setWording(string $wording): static
+    {
+        $this->wording = $wording;
+
+        return $this;
+    }
+}
