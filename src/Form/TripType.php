3f3938fe1d027978acdac42eff3a2aa9c9531979
KevinV2023 <kevin.viannenc2023@campus-eni.fr>
2024-02-06 16:36:55 +0100

    Ajout Trip formulaire

--- src/Form/TripType.php
+++ src/Form/TripType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\Trip;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,11 +21,36 @@ class TripType extends AbstractType
             ->add('idParticipant')
             ->add('name')
             ->add('dateTimeStart')
-            ->add('duration')
+            /*->add('duration', DateIntervalType::class, [
+                'widget' => 'choice',
+                'with_minutes' => true,
+                'minutes' => array_combine(range(1, 60), range(1, 60))
+            ])*/
+            ->add('duration', DateIntervalType::class, [
+                'widget' => 'choice',
+                'with_years' => false,
+                'with_months' => false,
+                'with_days' => true,
+                'days' => range(1, 32),
+                'with_hours' => true,
+                'hours' => range(1, 24),
+                'with_minutes' => true,
+                'minutes' => array_combine(range(1, 60), range(1, 60))
+            ])
             ->add('registrationDeadline')
             ->add('maxNumbRegistration')
-            ->add('tripInfo')
-            ->add('statut')
+            ->add('tripInfo', TextareaType::class)
+            ->add('statut', ChoiceType::class, [
+                'choices' => [
+                    '---Choice a status---' => 'choice',
+                    'Created' => 'Created',
+                    'Opened' => 'Opened',
+                    'Closed' => 'Closed',
+                    'Activity in course' => 'Activity in course',
+                    'Passed' => 'Passed',
+                    'Canceled' => 'Canceled'
+                ]
+            ])
         ;
     }
 
