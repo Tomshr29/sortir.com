059c37ce6ab1484e426ed66e59e474730dbc87fd
frpoteau <152607603+frpoteau@users.noreply.github.com>
2024-02-08 09:32:08 +0100

    add controller profil

--- src/Controller/ProfileModificationController.php
+++ src/Controller/ProfileModificationController.php
@@ -2,33 +2,92 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\EditUserPasswordType;
 use App\Form\ProfileModificationType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/profil", name: 'profile_')]
+#[Route('/user', name: 'user_')]
 class ProfileModificationController extends AbstractController
 {
-    #[Route("/", name: 'modification')]
-    public function modifie(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/profil/{id}', name: 'profil')]
+    public function modifie(int $id,UserRepository $userRepository,Request $request, EntityManagerInterface $entityManager): Response
     {
-        $user = $this->getUser();
+        $user = $userRepository->find($id);
 
-        $profileForm = $this->createForm(ProfileModificationType::class, $user);
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
 
-       $profileForm->handleRequest($request);
+        if ($this->getUser() !== $user){
+            return $this->redirectToRoute('app_listTrip');
+        }
 
-       if($profileForm->isSubmitted() && $profileForm->isValid()){
+        $form = $this->createForm(ProfileModificationType::class, $user);
+
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() && $form->isValid()){
+           $user = $form->getData();
            $entityManager->persist($user);
            $entityManager->flush();
+
+           $this->addFlash(
+               'success',
+               'Les informations de votre compte ont bien été modifiées'
+           );
+
        }
-
-
         return $this->render('profile/profile.html.twig', [
-            'profileForm' => $profileForm->createView()
+            'form_edit_profile' => $form->createView()
+        ]);
+    }
+
+    #[Route('profil/edition-mot-de-passe/{id}', name: 'edit_password')]
+    public function editPassword(int $id,UserRepository $userRepository,
+                                 Request $request, UserPasswordHasherInterface $hasher,EntityManagerInterface $entityManager) :Response
+    {
+        $user = $userRepository->find($id);
+
+        $form = $this->createForm(EditUserPasswordType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if($hasher->isPasswordValid($user, $form->getData()->getPlainPassword)){
+                $user->setPassword(
+                    $form->getData()->getNewPassword()
+                );
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été modifié'
+                );
+                return $this->redirectToRoute('user_profil');
+            }else {
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe est incorrect'
+                );
+            }
+        }
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Les informations de votre compte ont bien été modifiées'
+            );
+
+        return $this->render('profil/edit_password.html.twig', [
+            'form_edit_password'=> $form->createView()
         ]);
     }
 }
