567647636d3ce94086fd2ae64433309cac95bb96
frpoteau <152607603+frpoteau@users.noreply.github.com>
2024-02-15 17:36:44 +0100

    Ajout des tables SQL (Shape et Campus) à utiliser.

---
+++ public/Support/symfony-project.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO `campus` (`id`, `name`) VALUES
+(1, 'Nantes'),
+(2, 'Rennes'),
+(3, 'Quimper'),
+(4, 'Niort');
+
+-- --------------------------------------------------------
+
+INSERT INTO `shape` (`id`, `wording`) VALUES
+(1, 'En création'),
+(2, 'Ouvertes'),
+(3, 'Clôturées'),
+(4, 'En cours'),
+(5, 'Annulée');
+
+-- --------------------------------------------------------
